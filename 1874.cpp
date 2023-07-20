7ca8a6d0c4145107d2a331133c5b485d9eb5d510
Kevin Hyojoon Shin <39956521+Storkycold@users.noreply.github.com>
2023-07-20 12:24:47 +0900

    Add files via upload

---
+++ 1874.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int seq[100001];
+
+int main()
+{
+    int n;
+    int itr = 0;
+    vector<int> v;
+    vector<char> ans;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> seq[i];
+
+    for (int i = 1; i <= n; i++)
+    {
+        v.push_back(i);
+        ans.push_back('+');
+
+        while (!v.empty() && v.back() == seq[itr])
+        {
+            v.pop_back();
+            ans.push_back('-');
+            itr++;
+        }
+    }
+
+    if (!v.empty())
+        cout << "NO";
+    else
+    {
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << "\n";
+    }
+    return 0;
+}
